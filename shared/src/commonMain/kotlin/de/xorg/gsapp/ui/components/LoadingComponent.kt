8e7f6c758bd7a430227c8fd3c54e5e2cb42db54c
Fabian Schillig <xorgmc@gmail.com>
2023-09-19 15:27:41 +0200

    Make LoadingComponent centered vert&horiz

--- shared/src/commonMain/kotlin/de/xorg/gsapp/ui/components/LoadingComponent.kt
+++ shared/src/commonMain/kotlin/de/xorg/gsapp/ui/components/LoadingComponent.kt
@@ -1,11 +1,11 @@
 package de.xorg.gsapp.ui.components
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
@@ -13,7 +13,8 @@ fun LoadingComponent(
     modifier: Modifier = Modifier
 ) {
     Column(modifier = modifier,
-           verticalArrangement = Arrangement.Center) {
+           verticalArrangement = Arrangement.Center,
+           horizontalAlignment = Alignment.CenterHorizontally) {
         CircularProgressIndicator()
         Text("Wird geladen…")
     }
