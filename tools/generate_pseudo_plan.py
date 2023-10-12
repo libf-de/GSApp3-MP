5d78b386f8b8e9e25d077cc96cd1bd6727027b47
fsch <fsch@localhost.local>
2023-10-12 13:31:07 +0200

    Add tool to generate a sample substitution plan html

---
+++ tools/generate_pseudo_plan.py
@@ -0,0 +1,24 @@
+for ok in range(5, 11):
+    for uk in range(1, 6):
+        for st in range(1, 10):
+            print("      <tr>")
+            print("        <td class=\"vpTextZentriert\">" + str(ok) + "." + str(uk) + "</td>")
+            print("        <td class=\"vpTextZentriert\">" + str(st) + "</td>")
+            print("        <td class=\"vpTextZentriert\">Sn</td>")
+            print("        <td class=\"vpTextLinks\">##</td>")
+            print("        <td class=\"vpTextZentriert\">DMu1</td>")
+            print("        <td class=\"vpTextZentriert\">##</td>")
+            print("        <td class=\"vpTextZentriert\">Ausfall</td>")
+            print("      </tr>")
+
+for ak in range(15, 24):
+    for st in range(1, 10):
+        print("      <tr>")
+        print("        <td class=\"vpTextZentriert\">A" + str(ak) + "</td>")
+        print("        <td class=\"vpTextZentriert\">" + str(st) + "</td>")
+        print("        <td class=\"vpTextZentriert\">Sn</td>")
+        print("        <td class=\"vpTextLinks\">WEL</td>")
+        print("        <td class=\"vpTextZentriert\">DCk2</td>")
+        print("        <td class=\"vpTextZentriert\">If</td>")
+        print("        <td class=\"vpTextZentriert\">&nbsp;</td>")
+        print("      </tr>")
